dab43f47a6d25d633b87696da854a9cfa34fb77c
Sumonsnote <sohankhance@gmail.com>
2022-05-07 23:41:22 +0600

    register form validation

--- src/components/SharedFiles/Register.js
+++ src/components/SharedFiles/Register.js
@@ -4,6 +4,7 @@ import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../Firebase.init';
 import Loading from './Loading';
 import SocialLogin from './SocialLogin';
+import toast, { Toaster } from 'react-hot-toast';
 
 const Register = () => {
     const navigate = useNavigate()
@@ -22,7 +23,25 @@ const Register = () => {
             e.preventDefault()
             const email = e.target.email.value;
             const password = e.target.password.value
-            createUserWithEmailAndPassword(email, password)
+            const confirmPassword = e.target.confirmPassword.value
+            createUserWithEmailAndPassword(email, password, confirmPassword)
+
+            if (password !== confirmPassword) {
+                toast.error('Password did not match')
+                return;
+            }
+            if (email === '' || email === undefined) {
+                toast.error('Please enter an Email!');
+                return;
+            }
+            if (password === '' || password === undefined) {
+                toast.error('Please enter your Password!');
+                return;
+            }
+            if (confirmPassword === '' || confirmPassword === undefined) {
+                toast.error('Please enter your Password!');
+                return;
+            }
       }
 
     if (user) {
@@ -33,7 +52,7 @@ const Register = () => {
     }
     
     return (
-        <div>
+        <div style={{marginBottom: "62px"}}>
             <form onSubmit={handleRegister} className='w-25 container py-5'>
                 <h2 className='text-center'>Please Register</h2>
                 <div className="mb-3">
@@ -44,12 +63,16 @@ const Register = () => {
                     <label for="exampleInputPassword1" className="form-label">Password</label>
                     <input type="password" name='password' className="form-control" id="exampleInputPassword1" />
                 </div>
+                <div className="mb-3">
+                    <label for="exampleInputPassword2" className="form-label">Confirm password</label>
+                    <input type="password" name='confirmPassword' className="form-control" id="exampleInputPassword2" />
+                </div>
                 <button type="submit" className="btn btn-primary">Create Account</button>
                 <div className='d-flex justify-content-between py-5'>
                     <p>Already have an account?</p>
                     <Link to='/login'><p>Login</p></Link>
                 </div>
-                <SocialLogin></SocialLogin>
+                <SocialLogin></SocialLogin><Toaster></Toaster>
             </form>
             
         </div>
