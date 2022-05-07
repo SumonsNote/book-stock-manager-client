9fe83b1cd5f616fc48d8ad11d32ee3eb53248eb1
Sumonsnote <sohankhance@gmail.com>
2022-05-08 00:22:26 +0600

    ui modified

--- src/components/SharedFiles/Home.js
+++ src/components/SharedFiles/Home.js
@@ -11,45 +11,45 @@ import Items from './Items';
 const Home = () => {
     return (
         <div>
-            <img src={Banner} alt="" />
+            <img src={Banner} alt=""/>
             <div className='container'>
                 <Items></Items>
             </div>
             <div className='container'>
-                <div>
-                    <h2 className='py-5 text-uppercase text-center'>Customers</h2>
-                    <div className='customers-logo d-flex justify-content-center py-5'>
-                        <img src={rokomari} alt="" />
-                        <img src={wafilife} alt="" />
-                        <img src={kalantar} alt="" />
-                        <img src={maktabul} alt="" />
-                        <img src={niyamah} alt="" />
-                    </div>
+            <div>
+                <h2 className='py-5 text-uppercase text-center'>Customers</h2>
+                <div className='customers-logo d-flex justify-content-center py-5 bg-secondary bg-gradient text-white rounded-3'>
+                    <img src={rokomari} alt=""/>
+                    <img src={wafilife} alt=""/>
+                    <img src={kalantar} alt=""/>
+                    <img src={maktabul} alt=""/>
+                    <img src={niyamah} alt=""/>
                 </div>
-
-                <div>
-                    <h2 className='text-uppercase py-5 text-center'>Features</h2>
-                    <div className='d-flex justify-content-center flex-wrap'>
-                        <div className='features'>
+            </div>
+            
+            <div className='py-3'>
+                <h2 className='text-uppercase py-5 text-center'>Features</h2>
+                    <div className='d-flex justify-content-center flex-wrap bg bg-secondary text-white rounded-3'>
+                        <div className='features rounded-pill'>
                             <h2>Fastest Services</h2>
                         </div>
-                        <div className='features'>
+                        <div className='features rounded-pill'>
                             <h2>Authentic Products</h2>
                         </div>
-                        <div className='features'>
+                        <div className='features rounded-pill'>
                             <h2>Cash on Delivery</h2>
                         </div>
-                        <div className='features'>
+                        <div className='features rounded-pill'>
                             <h2>No Question Return policy</h2>
                         </div>
-                        <div className='features'>
+                        <div className='features rounded-pill'>
                             <h2>Delivery worldwide</h2>
                         </div>
-                        <div className='features'>
+                        <div className='features rounded-pill'>
                             <h2>100% customer satisfaction</h2>
                         </div>
                     </div>
-                </div>
+            </div>
             </div>
         </div>
     );
